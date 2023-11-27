b9753ca3402c7dd09e8a3bb885fa549bcca5aee5
Global727 <88992678+Global727@users.noreply.github.com>
2023-11-27 10:00:57 -0500

    Update home.js

--- public/home.js
+++ public/home.js
@@ -75,7 +75,7 @@ function BalanceForm(props){
             //props.setStatus(text);
             props.setShow(false);
             //setDegree(lang1);
-            props.setStatus((obj[0].name) +' '+ (obj[0].desc));
+            props.setStatus((obj.map((obj) => obj.name)));
             console.log('JSON:', text, obj, obj[0].name);
         } catch(err) {
             props.setStatus(text)
